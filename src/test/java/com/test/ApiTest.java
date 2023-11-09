bf155808d405a2cb2055cf42f71c5b206653138b
shreyasjoshi <kit.shreyasjoshi@gmail.com>
2023-11-09 08:08:50 +0530

    DGTL-API testing

--- src/test/java/com/test/ApiTest.java
+++ src/test/java/com/test/ApiTest.java
@@ -2,31 +2,30 @@ package com.test;
 
 import org.testng.annotations.Test;
 
+import com.base.BaseClass;
+
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import static org.hamcrest.Matchers.*;
 
-public class ApiTest {
+import java.io.File;
+
+public class ApiTest extends BaseClass {
 	
 	@Test
 	public void test() {
+		baseURI = "https://reqres.in/api";
 		
-	 given().get("http://localhost:8081/api/v1/employee").then()
-	 			.body("employeeName[0]",equalTo("shreyas jos"));
+		given().baseUri(baseURI).when().get("/users").then().assertThat().statusCode(200).log().all();
+	 
 	}
 	
 	
 	@Test
 	public void test1() {
-		
-		baseURI = "http://localhost:8081/api/v1";
-		
-		given()
-			.header("content-type","application/json")
-		.when()
-			.get("/employee")
-		.then()
-			.log().all();
+		baseURI = "https://reqres.in/api";
+		File file = new File(projectPath+"//src//test//resources//testdata//user.json");
+		given().baseUri(baseURI).body(file).when().post("/user").then().assertThat().statusCode(201).log().all();
 		
 	}
 
